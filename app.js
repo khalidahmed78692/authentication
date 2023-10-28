bcbb3b6dbf2dfee701d1b0ffbbe6f0538e9518f5
Khalid <khalidahmed78692@gmail.com>
2023-10-28 17:32:17 +0530

    add git ignore to the project

---
+++ app.js
@@ -0,0 +1,66 @@
+//jshint esversion:6
+
+require("dotenv").config();
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+mongoose.connect("mongodb://0.0.0.0:27017/userDB");
+
+console.log(process.env.API_KEY);
+
+const userSchema = new mongoose.Schema({
+  user: String,
+  password: String,
+});
+
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"],
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({ user: username }).then(function (found) {
+    if (found) {
+      if (found.password === password) res.render("secrets");
+    }
+  });
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.post("/register", function (req, res) {
+  const newUser = new User({
+    user: req.body.username,
+    password: req.body.password,
+  });
+  newUser.save().then(function () {
+    res.render("secrets");
+  });
+});
+
+app.listen(3000, function (req, res) {
+  console.log("Successfully running server on port 3000");
+});
